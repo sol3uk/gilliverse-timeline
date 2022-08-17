084eada1cfd93f7b86434f3c85b8b5ee669cc432
Ben A <ben@sol3.me>
2022-08-18 00:43:37 +0100

    do a bit more tidying & add some test data

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ const App = () => {
     width: 100%;
     height: 100vh;
     display: grid;
+    grid-template-rows: 20% 80%;
     align-content: center;
     justify-items: center;
     section{
@@ -40,25 +41,44 @@ const App = () => {
         background: #555; 
       }
     }
+    .goHThC {
+      padding: 0;
+    }
   `
   const LogoWrapper = styled.div`
     display: flex;
     justify-items: center;
     flex-direction: row;
     align-items: center;
-    max-width: 60vw;
+    max-width: 55vw;
+    @media (max-width: 900px) { 
+      flex-direction: column;
+    }
   `
   const items: TimelineItemModel[] = [
     {
       title: "Mid 60s",
-      cardTitle: "S3E10 Lantern",
+      cardTitle: "Season 3 Episode 10: Lantern",
+      cardSubtitle: "Better Call Saul",
       cardDetailedText: "Opening, Chuck reads to Jimmy from the Mabel book.",
     },
     {
       title: "1973",
       cardTitle: "S2E7 Inflatable",
-      cardSubtitle: "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardSubtitle: "Better Call Saul",
       cardDetailedText: "Jimmy sees his father get taken advantage of by a grifter.",
+    },
+    {
+      title: "Late 70s",
+      cardTitle: "S4E4 Talk",
+      cardSubtitle: "Better Call Saul",
+      cardDetailedText: "Mike builds a playground for Matty (stop once it jumps into the flashforward).",
+    },
+    {
+      title: "Early 80s",
+      cardTitle: "S1E3 …And the Bag's in the River",
+      cardSubtitle: "Breaking Bad",
+      cardDetailedText: "Walt and Gretchen talk about the composition of the human body in grad school.",
     },
   ];
 
@@ -74,12 +94,12 @@ const App = () => {
         mode="VERTICAL_ALTERNATING"
         enableOutline
         theme={{
-          primary: '#c1141c',
-          secondary: '#fef901',
+          primary: '#006d31',
+          secondary: '#c1141c',
           cardBgColor: 'gray',
           cardForeColor: 'white',
           titleColor: 'white',
-          titleColorActive: '#c1141c',
+          titleColorActive: '#fef901',
         }} />
     </TimelineWrapper>
 
